4d680436eb2c0d683f81c8aad5f33785aa82bed6
Luke Bamtefa <41142438+heisluke@users.noreply.github.com>
2023-04-15 03:44:51 +0100

    terminal algo

---
+++ age-calculator-app-main/terminal algorithm.js
@@ -0,0 +1,21 @@
+console.log("Welcome to Lou's Magic Age Calculator");
+
+let userName = prompt("What's your name?");
+let birthDate = new Date(prompt("MM/DD/YYYY"));
+let currentDate = new Date();
+let differenceInMilliseconds = currentDate.getTime() - birthDate.getTime();
+
+console.log("birthDate is: " + birthDate.getTime());
+console.log("currentDate is: " + currentDate.getTime());
+
+let millisecondsPerYear = 1000 * 60 * 60 * 24 * 365.25; //year answer
+let years = Math.floor(differenceInMilliseconds / millisecondsPerYear);
+
+let millisecondsPerMonth = 1000 * 60 * 60 * 24 * 30.44; // average month length
+let months = Math.floor((differenceInMilliseconds % millisecondsPerYear) / millisecondsPerMonth);
+
+let millisecondsPerDay = 1000 * 60 * 60 * 24; // day answer
+let days = Math.floor((differenceInMilliseconds % millisecondsPerMonth) / millisecondsPerDay);
+
+console.log("working magic 🔮🪄");
+console.log("Hmnmnn, " + userName + ", you are " + years + " years, " + months + " months, and " + days + " days old! 🪄😎");
